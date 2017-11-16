4401616fa5d2ddf89e1a557e082ccd75b46a284e
aldiws <syldie.aldiwijaya@gmail.com>
2017-11-16 13:53:18 +0700

    calculator done

--- calculator.js
+++ calculator.js
@@ -2,20 +2,38 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (input) {
+    this.input = 0
+    this.phi = Math.PI
+    
   }
-  add () {
+  add (numberAdd) {
+    this.input = this.input + numberAdd
+    return this
   }
-  substract () {
+  substract (numberSubstract) {
+    this.input = this.input - numberSubstract
+    return this
   }
-  multiply () {
+  multiply (numberMultiply) {
+    this.input = this.input * numberMultiply
+    return this
   }
-  divide () {
+  divide (numberDivide) {
+    this.input = this.input / numberDivide
+    return this
   }
-  square () {
+
+  square (numberSquare) {  
+    this.input = Math.pow(this.input, numberSquare)
+    return this
   }
+
   squareRoot () {
+    this.input = Math.sqrt(this.input)
+    return this
   }
+
 }
 
 /** note : you can use several features from ecmascript, such as:
@@ -25,6 +43,9 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+let addNumber = new Calculator()
+addNumber.add(9).substract(1).square(2)
+console.log(`Hasil : 9 - 1 pangkat 2 adalah ${addNumber.input} `)
 
 module.exports = {
   Calculator
