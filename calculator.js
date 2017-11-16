07350d8472559abeea8c638ee59108c8ee6447cf
Tobias Kusnaman <tobiaskusnaman@Tobiass-MacBook-Pro.local>
2017-11-16 14:11:21 +0700

    done

--- calculator.js
+++ calculator.js
@@ -3,18 +3,32 @@
 class Calculator {
   //write your code here
   constructor () {
+    this.result = 0;
   }
-  add () {
+  add (tambah) {
+    this.result += tambah
+    return this
   }
-  substract () {
+  substract (kurang) {
+    this.result -= kurang
+    return this
   }
-  multiply () {
+  multiply (kali) {
+
+    this.result = this.result * kali
+    return this
   }
-  divide () {
+  divide (bagi) {
+    this.result /= bagi
+    return this
   }
-  square () {
+  square (pangkat) {
+    this.result = Math.pow(this.result, pangkat)
+    return this
   }
-  squareRoot () {
+  squareRoot (akar) {
+    this.result = Math.sqrt(this.result)
+    return this
   }
 }
 
@@ -29,3 +43,8 @@ class Calculator {
 module.exports = {
   Calculator
 }
+
+let result = new Calculator();
+
+// console.log(result.substract(10));
+console.log(result.add(10).substract(2).multiply(2).divide(2).square(2).squareRoot());
