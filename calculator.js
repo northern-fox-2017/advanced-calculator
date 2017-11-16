ed243899dd920952c83b6c1656896e14f220b0fe
dhnlr <dhani.534@gmail.com>
2017-11-16 11:32:12 +0700

    Complete calculator class and add test case

--- calculator.js
+++ calculator.js
@@ -3,18 +3,34 @@
 class Calculator {
   //write your code here
   constructor () {
+    this.result = 0;
   }
-  add () {
+  add (num) {
+    this.result += num;
+    return this;
   }
-  substract () {
+  substract (num) {
+    this.result -= num;
+    return this;
   }
-  multiply () {
+  multiply (num) {
+    this.result *= num;
+    return this;
   }
-  divide () {
+  divide (num) {
+    this.result /= num;
+    return this;
   }
-  square () {
+  square (exponent) {
+    this.result = Math.pow(this.result, exponent);
+    return this;
   }
   squareRoot () {
+    this.result = Math.sqrt(this.result);
+    return this;
+  }
+  getResult () {
+    return `Hasilnya adalah ${this.result}`
   }
 }
 
@@ -25,6 +41,10 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+
+let valueLess = new Calculator()
+
+console.log(valueLess.add(5).substract(1).multiply(2).divide(2).squareRoot().square(2).getResult());
 
 module.exports = {
   Calculator
